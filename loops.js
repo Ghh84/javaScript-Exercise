f8c3bac0cd646a8aa9d1331f83c04558538340bd
Ghebrehiwet <ghiwethh@gmail.com>
2025-01-02 11:41:08 +0200

    Lab on loops.

--- loops.js
+++ loops.js
@@ -93,3 +93,68 @@ for (let movie of movies) {
         console.log(`${movie.title} - ${movie.rating}`);
     }
 }
+
+/*
+LAB 3 Loops
+Scenario
+
+We can improve our contact list program a bit by using loops. You can now try to display not only the
+ first or last contact, but all contacts in the list, regardless of their number.
+
+Additionally, try to enclose the whole program in a loop so that the user is repeatedly asked what
+ they want to do. The user can choose to:
+
+    display the first contact (first)
+    display the last contact (last)
+    display all contacts (all)
+    add a new contact (new)
+    exit the program (quit)
+
+After executing the selected action, the program will give the opportunity to choose again. The program 
+should end the actions only after the user gives a specified command, for example quit.
+*/
+
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "maxwell@ghh.fi",
+}, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "raja@vanttaa.fi",
+}, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "hellen,richards@rahsi.fi",
+}];
+let choice; 
+while (true) {
+    choice = prompt("Enter your choice", "first");
+    if (choice === "quit") {
+        break;
+    }
+    if (choice === "first") {
+        console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+    } else if (choice === "last") {
+        let last = contacts.length - 1;
+        console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+    } else if (choice === "all") {
+        for (let contact of contacts) {
+            console.log(`${contact.name} / ${contact.phone} / ${contact.email}`);
+        }
+    } else if (choice === "new") {
+        let newName = prompt("Name of new contact, enter the name ", "Rahsi");
+        let newPhone = prompt("Phone number, enter the number", "07450394");
+        let newEmail = prompt("Email , enter email", "abb@nokia.fi");
+        if (newName === null || newPhone === null || newEmail === null) {
+            console.log("Error");
+        } else {
+            let newContact = {
+                name: newName,
+                phone: newPhone,
+                email: newEmail
+            };
+            contacts.push(newContact);
+        }
+    }
+}
