5f09c8468912f5520e4c68ef706cf32346c28ec1
Ghebrehiwet <ghiwethh@gmail.com>
2025-01-02 11:13:59 +0200

    Exercise 3: using for .. of loop for displaying the even numbers from array.

--- loops.js
+++ loops.js
@@ -30,5 +30,28 @@ if (Number(firstNumber) > Number(lastNumber)) {
         firstNumber -= 10;
     }
 }
+/*
+Exercise 3: There are ten different numbers in the following numbers array:
 
+let numbers = [21, 45, 100, 12, 11, 78, 61, 4, 39, 22];
+
+Write a program that first writes out all these numbers on the console, then only those that are even 
+(hint: the remainder of dividing an even number by 2 is equal to 0), then only those that are larger 
+than 10 and at the same time smaller than 60.
+*/
+let numbers = [21, 45, 100, 12, 11, 78, 61, 4, 39, 22];
+//using for.. of loop
+for (let number of numbers) {
+    console.log(number);
+}
+for (let number of numbers) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+}
+for (let number of numbers) {
+    if (number > 10 && number < 60) {
+        console.log(number);
+    }
+}
 
