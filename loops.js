b655f6820e5277440a772035acb70d5ddd045ed0
Ghebrehiwet <ghiwethh@gmail.com>
2025-01-02 11:06:12 +0200

    Exercise2: using prompt and and conditional on decision of the loop in javascript.

--- loops.js
+++ loops.js
@@ -13,5 +13,22 @@
     for (let i = 100; i >= 0; i -= 10) {
         console.log(i);
     }
-    
+
+/*
+Exercise 2: Modify the previous program so that it asks the user for the first and last number 
+it uses instead of 100 and 0 (hint: use the prompt dialog).
+
+Check if the entered values are correct (that the initial value is greater than the final value).
+ */
+let firstNumber = prompt("Enter the first number", "100");
+let lastNumber = prompt("Enter the last number", "0");
+if (Number(firstNumber) > Number(lastNumber)) {
+    console.log("Error");
+} else {
+    while (Number(firstNumber) >= Number(lastNumber)) {
+        console.log(firstNumber);
+        firstNumber -= 10;
+    }
+}
+
 
