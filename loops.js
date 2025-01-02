6f3aac0e969dbba7491df39406ee6783e3e09c90
Ghebrehiwet <ghiwethh@gmail.com>
2025-01-02 11:22:22 +0200

    Exercise 4: entering and displaying movies using array, conditional statement and loop.

--- loops.js
+++ loops.js
@@ -54,4 +54,42 @@ for (let number of numbers) {
         console.log(number);
     }
 }
+/*
 
+Exercise 4: Write a program using a loop that will ask the user for the name of the movie (first prompt)
+ and its rating from www.imdb.com (second prompt). The program will allow you to enter as many movies as
+you want into the movies array. Each element of the array will be an object, consisting of two fields:
+title and imdb. The input is completed if the user presses Cancel in the prompt dialog. Then the program
+should first print out to the console all movies that have a rating of less than 7, then those whose 
+rating is greater than or equal to 7. Write the name of the movie and its rating next to each other, e.g.:
+*/
+
+let movies = [];
+let title;
+let rating;
+let movie;
+while (true) {
+    title = prompt("Enter the name of the movie", "Titanic");
+    if (title === null) {
+        break;
+    }
+    rating = prompt("Enter the rating of the movie", "7.8");
+    if (rating === null) {
+        break;
+    }
+    movie = {
+        title: title,
+        rating: rating
+    };
+    movies.push(movie);
+}
+for (let movie of movies) {
+    if (movie.rating < 7) {
+        console.log(`${movie.title} - ${movie.rating}`);
+    }
+}
+for (let movie of movies) {
+    if (movie.rating >= 7) {
+        console.log(`${movie.title} - ${movie.rating}`);
+    }
+}
